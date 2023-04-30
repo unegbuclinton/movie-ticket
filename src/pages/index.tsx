ef939f08a5f0427d0456c2bbb8c849a5b2a09b3c
Clinton <unegbuclinton5@gmail.com>
2023-04-30 01:49:09 +0100

    fix deployment issue

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,10 @@
 import { playingMovie, topMovie, tvMovies } from "@/utils/redux/MovieSlice";
 import MoviesPage from "./moviepage";
 import { useAppDispatch } from "@/utils/hooks";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [delay, setDelay] = useState<boolean>(false);
   const dispatch = useAppDispatch();
   useEffect(() => {
     dispatch(playingMovie());
@@ -11,9 +12,11 @@ export default function Home() {
     dispatch(topMovie());
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
-  return (
-    <>
-      <MoviesPage />
-    </>
-  );
+
+  useEffect(() => {
+    const timer = setTimeout(() => setDelay(true), 3000);
+    return () => clearTimeout(timer);
+  }, []);
+
+  return <>{delay && <MoviesPage />}</>;
 }
